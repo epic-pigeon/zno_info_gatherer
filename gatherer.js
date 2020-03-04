477fad1ee377fcc67f0c13b85a2e2e918d81c4be
nikita202 <nikita@ostaplyuk.com>
2020-03-04 21:03:20 +0200

    Initial commit

---
+++ gatherer.js
@@ -0,0 +1,88 @@
+const https = require("https");
+const path = require("path");
+const zlib = require("zlib");
+
+//const KeyEncoder = require('key-encoder').default;
+//let keyEncoder = new KeyEncoder('secp256k1');
+
+const rootCas = require('ssl-root-cas').create();
+
+let HeadersGenerator = () => ({
+    headers: {},
+    setRequestHeader(name, value) {
+        this.headers[name] = value;
+    }
+});
+
+
+
+rootCas.addFile(path.resolve(__dirname, 'intermediate.pem'));
+
+https.globalAgent.options.ca = rootCas;
+
+function gather(email) {
+    return new Promise(((resolve, reject) => {
+        let payload = "email=" + encodeURIComponent(email);
+
+        let headersGenerator = HeadersGenerator();
+
+        headersGenerator.setRequestHeader("Accept", "application/json, text/javascript, */*;q=0.01");
+        headersGenerator.setRequestHeader("Accept-Encoding", "gzip, deflate, br");
+        headersGenerator.setRequestHeader("Accept-Language", "en-US,en;q=0.5");
+        headersGenerator.setRequestHeader("Cache-Control", "no-cache");
+        headersGenerator.setRequestHeader("Connection", "keep-alive");
+        headersGenerator.setRequestHeader("Content-Length", Buffer.byteLength(payload, "utf8"));
+        headersGenerator.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        headersGenerator.setRequestHeader("Host", "zno-kharkiv.org.ua");
+        headersGenerator.setRequestHeader("Origin", "https://zno-kharkiv.org.ua");
+        headersGenerator.setRequestHeader("Pragma", "no-cache");
+        headersGenerator.setRequestHeader("Referer", "https://zno-kharkiv.org.ua/register");
+        headersGenerator.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+
+        let options = {
+            host: "zno-kharkiv.org.ua",
+            port: 443,
+            path: "/register/api/recovery",
+            method: "POST",
+            headers: headersGenerator.headers,
+            //key: keyEncoder.encodePublic(certificate.pubkey.toString('hex'), "raw", "pem"),
+            //cert: certificate.pemEncoded,
+        };
+
+        let req = https.request(options, function (res) {
+            //console.log("statusCode: ", res.statusCode);
+            //console.log("headers: ", res.headers);
+
+            if (res.statusCode !== 200) reject(new Error("Bad status code " + res.statusCode));
+
+            //res.setEncoding("raw");
+
+            let chunks = [];
+
+            res.on("data", chunks.push.bind(chunks));
+            res.on("end", () => {
+                let data = Buffer.concat(chunks);
+                //console.log(data);
+                zlib.gunzip(data, function (err, result) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    let parsed = JSON.parse(result.toString());
+                    if (parsed.status === "success") {
+                        resolve({
+                            personalCode: parsed.cu_pers_code,
+                            pin: parsed.cu_pin
+                        });
+                    } else reject(new Error("Server error or no user found"));
+                });
+            });
+            res.on("error", reject)
+        }).on("error", reject);
+
+        req.write(Buffer.from(payload).toString("utf8"));
+        req.end();
+    }));
+}
+
+module.exports = gather;
---
+++ index.js
@@ -0,0 +1,47 @@
+//const sslCertificate = require('get-ssl-certificate');
+
+const gather = require("./gatherer");
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+http.createServer((req, res) => {
+    let {query} = url.parse("http://localhost:1337" + req.url);
+    let parsedQuery = {};
+    if (query) {
+        parsedQuery = Object.assign({},
+            ...query.split("&").map(val => {
+                if (val.indexOf("=") === -1) {
+                    let result = {};
+                    result[decodeURIComponent(val)] = "";
+                    return result;
+                } else {
+                    let key = decodeURIComponent(val.split("=")[0]);
+                    let value = decodeURIComponent(val.split("=").slice(1).join("="));
+                    let result = {};
+                    result[key] = value;
+                    return result;
+                }
+            })
+        );
+    }
+    if (parsedQuery.email) {
+        //console.log(parsedQuery.email);
+        gather(parsedQuery.email).then(result => {
+            res.writeHead(200, {});
+            res.end(JSON.stringify({success: true, ...result}));
+        }).catch(e => {
+            res.writeHead(200, {});
+            res.end(JSON.stringify({success: false, message: e.toString()}));
+        })
+    } else {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        fs.readFile("./index.html", "UTF-8", (err, result) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            res.end(result.toString());
+        });
+    }
+}).listen(1337);
